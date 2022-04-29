dbb5c1f4f58545295db204ce655cbb6b3740da7c
Rafael Aguiar <rafaelpaguiar@gmail.com>
2022-04-29 16:36:02 -0300

    Inserção de usuário com POST

--- src/main/java/br/com/bettatit/workshopmongo/services/UserService.java
+++ src/main/java/br/com/bettatit/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.bettatit.workshopmongo.dto.UserDTO;
 import br.com.bettatit.workshopmongo.entities.User;
 import br.com.bettatit.workshopmongo.repositories.UserRepository;
 import br.com.bettatit.workshopmongo.services.exception.ObjectNotFoundException;
@@ -24,5 +25,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
