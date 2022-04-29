fe76908cc49ba785498abcc5cff7f0b7615c01e0
Rafael Aguiar <rafaelpaguiar@gmail.com>
2022-04-29 16:17:56 -0300

    Obtendo um usuário por id

--- src/main/java/br/com/bettatit/workshopmongo/services/UserService.java
+++ src/main/java/br/com/bettatit/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package br.com.bettatit.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.bettatit.workshopmongo.entities.User;
 import br.com.bettatit.workshopmongo.repositories.UserRepository;
+import br.com.bettatit.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,4 +21,8 @@ public class UserService {
 		return repository.findAll();
 	}		
 	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
 }
