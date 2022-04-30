d0b7d136ddd4caafc4ecc62fc5738fa053ab1d3a
Rafael Aguiar <rafaelpaguiar@gmail.com>
2022-04-30 12:31:20 -0300

    Consulta simples com @Query

--- src/main/java/br/com/bettatit/workshopmongo/repositories/PostRepository.java
+++ src/main/java/br/com/bettatit/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package br.com.bettatit.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.bettatit.workshopmongo.entities.Post;
@@ -11,5 +12,8 @@ import br.com.bettatit.workshopmongo.entities.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 }
